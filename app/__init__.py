8fe62f99689242b17d45dcc840c04261d35a9be6
David Naki <dlnaki1160@gmail.com>
2025-05-06 21:52:02 +0300

    arrange the code with router, service and repo

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ def create_app():
     app = Flask(__name__)
 
 
-    app.register_blueprint(test_bp, url_prefix='/test')
+    # app.register_blueprint(test_bp, url_prefix='/test')
+    app.register_blueprint(test_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,50 +1,26 @@
-from flask import Blueprint, jsonify, request
-from .db_utils import Database
+import http
+from flask import Blueprint, request
+from .services import service_create_user, service_get_all_useres
 
-
-test_route = Blueprint('test', __name__)
+test_route = Blueprint('user_test', __name__)
 
 
 # GET - 
-@test_route.route('/', methods=['GET'])
-def get_test():
+@test_route.route('/test', methods=['GET'])
+def get_user_test():
     try:
-        db = Database()
-        data = db.execute_query("SELECT * FROM users")
-        print(f"data is: { data }")
-        # return {'data': data}, 200
-        return jsonify(data), 200
+        return service_get_all_useres(), http.HTTPStatus.OK
+    
     except Exception as e: 
-        return {'error': str(e)}, 500
+        return {'error': str(e)}, http.HTTPStatus.INTERNAL_SERVER_ERROR
 
 
 # POST -
-@test_route.route('/', methods=['POST'])
-def post_test():
+@test_route.route('/test', methods=['POST'])
+def create_user_test():
     try:
-        db = Database()
-        data = request.get_json()
-        print(f"data is: { data }")
+        print(f"data is: { request.get_json() }")
+        return service_create_user(request.get_json()), http.HTTPStatus.CREATED
 
-        first_name = data.get('first_name')
-        last_name = data.get('last_name')
-        email = data.get('email')
-
-        if not first_name or not last_name or not email:
-            return {'error': 'Missing required fields'}, 400
-        
-        # Assuming you have a function to insert data into the database
-        insert_query = """
-            INSERT INTO users (first_name, last_name, email)
-            VALUES (%s, %s, %s)
-        """
-        db.execute_query(insert_query, (first_name, last_name, email))
-        # return the new data as a response 
-        return {
-            'first_name': first_name,
-            'last_name': last_name,
-            'email': email
-        }
-        # return {'message': 'Data inserted successfully'}, 201
     except Exception as e:
-        return {'error': str(e)}, 500
+        return {'error': str(e)}, http.HTTPStatus.INTERNAL_SERVER_ERROR
