5f9b2967e20703cb4dbdf4ee8bb696bbacf858cf
David Naki <dlnaki1160@gmail.com>
2025-05-06 23:14:27 +0300

    delete the test route

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
-from .test_routes import test_bp
 from .user_routes import user_bp
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.register_blueprint(test_bp, url_prefix='/test')
     app.register_blueprint(user_bp, url_prefix='/users')
 
     return app
